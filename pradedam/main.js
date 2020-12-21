7f88f8c6438c389c2f41f56b4a6f6bf9de686332
Vilimax <viliuseidukonis@gmail.com git config --global user.name Vilimax>
2020-12-21 20:07:41 +0200

    11pamoka

---
+++ pradedam/main.js
@@ -0,0 +1,25 @@
+'use strict'
+
+
+
+
+
+let arrayVar = [10, 20, 30];
+
+function changeColorRed() {
+    console.log('changeColorRed');
+    document.getElementById('container').style.backgroundColor = 'red';
+    
+}
+function changeColorGreen() {
+    console.log('changeColorGreen');
+    document.getElementById('container').style.backgroundColor = 'green';
+    
+}
+
+
+
+
+
+
+
